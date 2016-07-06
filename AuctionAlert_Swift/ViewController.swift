1d0f0ef0ce8153590401353198ee871c7a9013b4
Robin Spinks <uglybluecat@googlemail.com>
2016-07-06 20:38:31 +0100

    objects created om initial view

--- AuctionAlert_Swift/ViewController.swift
+++ AuctionAlert_Swift/ViewController.swift
@@ -9,17 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let topMargin: CGFloat = 20.0
+    let margin: CGFloat = 10.0
+    let standardControlWidth: CGFloat = 200.0
+    let standardControlHeight: CGFloat = 30.0
+    
+    var titleLabel: UILabel!
+    var realmEntry: UITextField!
+    var objectEntry: UITextField!
+    var searchButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        setupView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    func setupView() {
+        self.view.backgroundColor = UIColor(red: 123/256, green: 31/256, blue: 162/256, alpha: 1)
+        addObjects()
+    }
+    
+    func addObjects() {
+        let titleLabelFrame: CGRect = CGRect(x: margin,
+                                             y: topMargin,
+                                             width: self.view.bounds.size.width - 2*margin,
+                                             height: standardControlHeight)
+        titleLabel = UILabel(frame: titleLabelFrame)
+        titleLabel.text = "Auction Alert"
+        titleLabel.textColor = UIColor.whiteColor()
+        titleLabel.textAlignment = .Center
+        self.view.addSubview(titleLabel)
+        
+        let realmEntryFrame: CGRect = CGRect(x: margin,
+                                             y: CGRectGetMaxY(titleLabelFrame) + margin,
+                                             width: self.view.bounds.size.width - 2*margin,
+                                             height: standardControlHeight)
+        realmEntry = UITextField(frame: realmEntryFrame)
+        realmEntry.placeholder = "Realm"
+        self.view.addSubview(realmEntry)
+        
+        let objectEntryFrame: CGRect = CGRect(x: margin,
+                                              y: CGRectGetMaxY(realmEntryFrame) + margin,
+                                              width: self.view.bounds.size.width - 2*margin,
+                                              height: standardControlHeight)
+        objectEntry = UITextField(frame: objectEntryFrame)
+        objectEntry.placeholder = "Object"
+        self.view.addSubview(objectEntry)
+        
+        let searchButtonFrame: CGRect = CGRect(x: (self.view.bounds.size.width - standardControlWidth) / 2,
+                                               y: self.view.bounds.size.height - margin - standardControlHeight,
+                                               width: standardControlWidth,
+                                               height: standardControlHeight)
+        searchButton = UIButton(frame: searchButtonFrame)
+        searchButton.titleLabel?.text = "Search"
+        searchButton.titleLabel?.textColor = UIColor.whiteColor()
+        searchButton.titleLabel?.backgroundColor = UIColor(red: 0, green: 188/256, blue: 212/256, alpha: 1)
+        self.view.addSubview(searchButton)
+    }
+    
+    
 }
-
