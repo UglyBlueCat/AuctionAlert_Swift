3c9de7d7797d2df27c35796cca5c2c81e5808975
Robin Spinks <uglybluecat@googlemail.com>
2016-08-11 16:57:18 +0100

    light status bar

--- AuctionAlert_Swift/AppDelegate.swift
+++ AuctionAlert_Swift/AppDelegate.swift
@@ -59,6 +59,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - Standard AppDelegate Methods
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        application.statusBarStyle = .LightContent
         
         setUserDefaults()
         registerForPushNotifications(application)
