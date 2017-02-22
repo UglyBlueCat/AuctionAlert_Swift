dd636324f2699c5850a74b7f309e0c700e60337d
Robin Spinks <uglybluecat@googlemail.com>
2017-02-22 14:46:42 +0000

    code re-factoring

--- AuctionAlert_Swift/DataHandler.swift
+++ AuctionAlert_Swift/DataHandler.swift
@@ -31,48 +31,93 @@ class DataHandler {
     
     /**
      Separates a JSON Array into JSON Dictionary objects.
-     Passes each object through extractValuesFromJSON before adding to searchResults property
      
      - parameter jsonData: The JSON Array
      */
     func populateResults (jsonData: AnyObject) {
         if let resultData = jsonData as? NSDictionary {
-            if let realmData = resultData["realms"] as? NSArray { // It'll be realm data from battle.net
-                realmList.removeAll()
-                for object in realmData {
-                    if let dictionaryObject = object as? NSDictionary,
-                        let realmName = dictionaryObject["name"] as? String
-                    {
-                        realmList.append(realmName)
-                    }
-                }
-                NotificationCenter.default.post(name: Notification.Name(rawValue: "kRealmsReceived"), object: nil)
-            } else if let message = resultData["message"] as? String { // It'll be an API response message
-                NotificationCenter.default.post(name: Notification.Name(rawValue: "kMessageReceived"),
-                                                                          object: nil,
-                                                                          userInfo: ["message" : message])
-            } else if let iconImage = resultData["icon"] as? String { // It'll be item data from Blizzard
-                if let itemID = resultData["id"] as? Int {
-                    ImageFetcher.sharedInstance.downloadImage(code: String(itemID), name: iconImage)
-                }
-            } else if let code = resultData["code"] as? Int { // It'll be a code check result
-                API_Interface.sharedInstance.fetchObjectData(code: String(code))
-                NotificationCenter.default.post(name: Notification.Name(rawValue: "kCodeOK"), object: nil)
+            if let realmData = resultData["realms"] as? NSArray {
+                self.populateRealmData(realmData: realmData)
+            } else if let message = resultData["message"] as? String {
+                self.handleMessage(message: message)
+            } else if let iconImage = resultData["icon"] as? String {
+                self.populateItemData(iconImage: iconImage, resultData: resultData)
+            } else if let code = resultData["code"] as? Int {
+                self.handleCodeCheckResult(code: code)
             }
-        } else if let resultData = jsonData as? NSArray { // It'll be search results from the AuctionAlert API
-            searchResults.removeAll()
-            for object in resultData {
-                let result: [String: AnyObject] = self.extractValuesFromJSON(object: object as AnyObject, values: ["item", "owner", "buyout", "bid", "quantity", "message", "code", "locale", "object", "price", "realm"])
-                searchResults.append(result)
-            }
-            NotificationCenter.default.post(name: Notification.Name(rawValue: "kDataReceived"), object: nil)
-        } else if let message = jsonData["message"] as? String { // It'll be an API response message
-            NotificationCenter.default.post(name: Notification.Name(rawValue: "kMessageReceived"),
-                                                                      object: nil,
-                                                                      userInfo: ["message" : message])
+        } else if let resultData = jsonData as? NSArray {
+            self.populateSearchResults(resultData: resultData)
+        } else if let message = jsonData["message"] as? String {
+            self.handleMessage(message: message)
         } else {
             DLog("Cannot convert data")
         }
+    }
+    
+    /**
+     Initiates download of an objects item data from Blizzard and notifies the system that the code was OK
+     
+     - parameter code: The code that was checked
+     */
+    func handleCodeCheckResult (code: Int) {
+        API_Interface.sharedInstance.fetchObjectData(code: String(code))
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "kCodeOK"), object: nil)
+    }
+    
+    /**
+     Downloads the icon for an item after that items data is received from Blizzard
+     
+     - parameters:
+        - iconImage: The name of the icon image
+        - resultData: The data containing the id for the image
+     */
+    func populateItemData (iconImage: String, resultData: NSDictionary) {
+        if let itemID = resultData["id"] as? Int {
+            ImageFetcher.sharedInstance.downloadImage(code: String(itemID), name: iconImage)
+        }
+    }
+    
+    /**
+     Notifies the system that a response message was received from the API
+     
+     - parameter message: The message
+     */
+    func handleMessage (message: String) {
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "kMessageReceived"),
+                                        object: nil,
+                                        userInfo: ["message" : message])
+    }
+    
+    /**
+     Populates the realm list with data from battle.net
+     
+     - parameter realmData: The list of realms
+     */
+    func populateRealmData (realmData: NSArray) {
+        realmList.removeAll()
+        for object in realmData {
+            if let dictionaryObject = object as? NSDictionary,
+                let realmName = dictionaryObject["name"] as? String
+            {
+                realmList.append(realmName)
+            }
+        }
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "kRealmsReceived"), object: nil)
+    }
+    
+    
+    /**
+     Populates search results from the AuctionAlert API
+     
+     - parameter resultData: The search results
+     */
+    func populateSearchResults (resultData: NSArray) {
+        searchResults.removeAll()
+        for object in resultData {
+            let result: [String: AnyObject] = self.extractValuesFromJSON(object: object as AnyObject, values: ["item", "owner", "buyout", "bid", "quantity", "message", "code", "locale", "object", "price", "realm"])
+            searchResults.append(result)
+        }
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "kDataReceived"), object: nil)
     }
     
     /**
@@ -92,7 +137,7 @@ class DataHandler {
             } else if let itemInt = object[elementName] as? Int {
                 result.updateValue(itemInt as AnyObject, forKey: elementName)
             } else {
-                // Assume element doesn't exist
+                DLog("Element \(elementName) doesn't exist")
             }
         }
         if result.count < 1 {
