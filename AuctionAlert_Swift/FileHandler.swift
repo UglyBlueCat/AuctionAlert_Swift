0f5c17a3e99999a2dbaa2e4e1817b0162cc0fd23
Robin Spinks <uglybluecat@googlemail.com>
2017-05-31 17:26:43 +0100

    FileHandler completed

--- AuctionAlert_Swift/FileHandler.swift
+++ AuctionAlert_Swift/FileHandler.swift
@@ -9,46 +9,59 @@
 import Foundation
 
 class FileHandler {
-    var fileURL : URL
     
-    init() {
-        fileURL = URL(string: "")!
-        createFile() // Want to throw error
+    enum FileHandlerError: Error {
+        case findPathError
+        case appendFolderError(folder : String)
     }
     
-    private func createFile() {
-        let fileNamed = "AuctionAlert"
+    var fileURL : URL = URL(string: "")!
+    
+    init(fileName : String) throws {
+        do {
+            try createFileURL(fileName)
+        } catch {
+            throw error
+        }
+    }
+    
+    private func createFileURL(_ fileName : String) throws {
         let folder = "AuctionAlert"
         
         guard let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first else {
-            DLog("Cannot establish path") // Want to throw error
-            return
+            throw FileHandlerError.findPathError
         }
         
         guard let writePath = NSURL(fileURLWithPath: path).appendingPathComponent(folder) else {
-            DLog("Cannot create folder \(folder)") // Want to throw error
-            return
+            throw FileHandlerError.appendFolderError(folder: folder)
         }
         
         do {
             try FileManager.default.createDirectory(atPath: writePath.path, withIntermediateDirectories: true)
         } catch {
-            DLog("Error: \(error) creating file at path: \(path)") // Want to throw error
-            return
+            throw error
         }
         
-        fileURL = writePath.appendingPathComponent(fileNamed + ".txt")
+        fileURL = writePath.appendingPathComponent(fileName)
     }
     
-    func write(_ text : String) {
+    func write(_ text : String) throws {
         do {
             try text.write(to: fileURL, atomically: false, encoding: String.Encoding.utf8)
         } catch {
-            DLog("Error: \(error) writing to file at path: \(fileURL)") // Want to throw error
+            throw error
         }
     }
     
-    func read() {
+    func read() throws -> String {
+        var readString : String
         
+        do {
+            readString = try String(contentsOf: fileURL)
+        } catch {
+            throw error
+        }
+        
+        return readString
     }
 }
